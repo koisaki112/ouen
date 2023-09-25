8cdd798615037e7a2f10302b7fd63ce9edc80ac8
koisaki112 <koisaki112@gmail.com>
2023-09-25 23:38:46 +0900

    fast commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const fan = document.getElementById("fan");
+const but = document.getElementById("but");
+
+fan.onclick = function(){
+    alert("ありがとう！あなたにも幸せが訪れますように！");
+    location.replace("https://twitter.com/koisaki112");
+}
+
+but.onclick = function(){
+    alert("何で？応援してよ");
+    alert("応援するくらいただじゃん");
+    alert("はいDMで応援メッセージ送ってください");
+    location.replace("https://twitter.com/koisaki112");
+}
